fe883dc0baad03089edd422075303cefc1a30df5
Ernest Mpiani <ernestmpiani15@gmail.com>
2025-09-20 12:43:59 +0000

    No description

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,8 +1,19 @@
 import WelcomeMessage from "./components/WelcomeMessage"
+// import Header from "./components/Header";
+// import MainContent from "./components/MainContent";
+// import Footer from "./components/Footer";
+
+import Header from "./Header";
+import MainContent from "./MainContent";
+import Footer from "./Footer";
+
 
 function App() {
   return (
-    <WelcomeMessage />
+    <>
+      <WelcomeMessage />
+    <Header />, <MainContent />, <Footer />
+    </>
   )
 }
 
